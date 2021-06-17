802d48aa7afcc6941e0e6507721431229d1706b6
Adrian Garza <adrianhgarza@gmail.com>
2021-06-16 20:28:20 -0700

    target dragging

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CameraControls : MonoBehaviour {
+	Vector3 mouseDragStart;
+	Vector3 positionOnDragStart;
+	Tilemap tilemap;
+
+	void Start() {
+		tilemap = GameObject.FindObjectOfType<Tilemap>();
+	}	
+
+    void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			mouseDragStart = Input.mousePosition;
+			positionOnDragStart = transform.position;
+		}
+
+		if (Input.GetMouseButton(0)) {
+			Vector3 worldDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.ScreenToWorldPoint(mouseDragStart);
+			Vector3 targetPos = positionOnDragStart - worldDelta;
+
+			Vector3 tilePos = targetPos;
+			tilePos.z = tilemap.transform.position.z;
+
+			if (tilemap.localBounds.Contains(tilePos/2f)) {
+				transform.position = positionOnDragStart - worldDelta;
+			} 
+		}
+    }
+}
