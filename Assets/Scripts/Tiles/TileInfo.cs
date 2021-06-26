ee3f988a995cc1982d6e36c80a32d2b580a66007
Adrian Garza <adrianhgarza@gmail.com>
2021-06-26 13:48:59 -0700

    fixed bubble prefab

---
+++ Assets/Scripts/Tiles/TileInfo.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TileInfo : MonoBehaviour {
+	#pragma warning disable 0649
+	[SerializeField] Text ageText;
+	[SerializeField] Text tileName;
+	#pragma warning restore 0649
+}
