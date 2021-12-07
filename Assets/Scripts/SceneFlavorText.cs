6653936ba7b8f0c5fa92d8bfce305fc59f6a219c
Adrian Garza <adrianhgarza@gmail.com>
2021-12-07 12:43:24 -0800

    show current streak in letter

--- Assets/Scripts/SceneFlavorText.cs
+++ Assets/Scripts/SceneFlavorText.cs
@@ -20,8 +20,11 @@ public class SceneFlavorText : MonoBehaviour {
 	#pragma warning disable 0649
 
 	Levels levels;
+	string originalLetterText;
+	bool hasSustainCriterion;
 
 	void Start() {
+		dayTracker = GameObject.FindObjectOfType<DayTracker>();
 		letterText.text = $"Level {GameObject.FindObjectOfType<Levels>().GetLevelNumber()+1}: {SceneManager.GetActiveScene().name}";
 
 		letterText.text += "\n\n"+this.text;
@@ -29,18 +32,29 @@ public class SceneFlavorText : MonoBehaviour {
 		WinCondition win = GameObject.FindObjectOfType<WinCondition>();
 		letterText.text += "\n\n" + win.GetDescription();
 
+		originalLetterText = letterText.text;
+
 		letter.SetActive(true);
 
 		levels = GameObject.FindObjectOfType<Levels>();
 		nextLevelButton.SetActive(false);
 		reloadButton.SetActive(false);
+		hasSustainCriterion = GameObject.FindObjectOfType<SustainCriterion>() != null;
+	}
+
+	public void OnDayEnd() {
+		if (hasSustainCriterion && !won) {
+			letterText.text = originalLetterText;
+			int d = dayTracker.GetDaysWithoutActions();
+			letterText.text += "\n\nCurrent streak: " + d + (d == 1 ? " day." : " days.");
+		}
 	}
 
 	public void OnWin() {
 		if (lost) return;
 		won = true;
 
-		DayTracker dayTracker = GameObject.FindObjectOfType<DayTracker>();
+		letterText.text = originalLetterText;
 		letterText.text += "\n\nComplete!";
 		letterText.text += $"\nTook <color='#7a09fa'>{dayTracker.GetTotalDays()}</color> days & <color='#7a09fa'>{dayTracker.GetTotalActions()}</color> actions.";
 		letter.SetActive(true);
